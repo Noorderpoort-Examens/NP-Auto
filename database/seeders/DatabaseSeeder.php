427f1ab336e7699680aaa9774a8036453edb73ba
DanielD <ddobma1@st.noorderpoort.nl>
2025-05-13 11:59:57 +0200

    Add seeding data for admin role and manage user permissions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +16,16 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'development',
             'email' => 'test@test.com',
             'password' => 'development'
         ]);
+
+        $role = Role::create(['name' => 'admin']);
+        $permission = Permission::create(['name' => 'manage users']);
+
+        $role->givePermissionTo($permission);
+        $user->assignRole($role);
     }
 }
