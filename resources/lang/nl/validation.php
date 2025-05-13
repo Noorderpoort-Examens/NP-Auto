58b5bf2a603040e5c8bab1211c81149a95573909
DanielD <ddobma1@st.noorderpoort.nl>
2025-05-13 14:10:42 +0200

    Add translation for validation

---
+++ resources/lang/nl/validation.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'required' => 'Het veld :attribute is verplicht.',
+    'email' => 'Het veld :attribute moet een geldig e-mailadres zijn.',
+    'unique' => 'Het :attribute is al in gebruik.',
+];
