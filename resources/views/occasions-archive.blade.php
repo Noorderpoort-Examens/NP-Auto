f4b7faaaa616a52b570c71ce1a7aa3c018f98768
DanielD <ddobma1@st.noorderpoort.nl>
2025-05-15 09:12:28 +0200

    Created hardcoded list of occasions Add default hardcoded car image

---
+++ resources/views/occasions-archive.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('title', 'Occasions Archive')
+
+@section('content')
+    <section class="py-8 antialiased bg-np-dark md:py-12">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-4">
+                @for ($i = 0; $i < 25; $i++)
+                    <div class="relative rounded-lg border p-6 shadow-sm border-gray-700 bg-np-dark/40 h-max">
+                        <a href="#" class="block group">
+                            <div
+                                class="relative overflow-hidden rounded-lg transform transition duration-150 ease-in-out group-hover:scale-110">
+                                <img src="{{ asset('img/a1_1.jpeg') }}" alt="logo" class="block w-full rounded-lg">
+                            </div>
+                        </a>
+
+                        <div class="pt-6">
+                            <p
+                                class="text-sm md:text-base lg:text-lg font-semibold leading-tight text-gray-900 hover:underline dark:text-white">
+                                Audi A3 Sportback
+                            </p>
+                            <p
+                                class="text-sm md:text-base lg:text-lg font-semibold leading-tight text-gray-900 hover:underline dark:text-white">
+                                €20.000
+                            </p>
+
+                            <div class="mt-4 flex items-center justify-between gap-4">
+                                <button type="button"
+                                    class="items-center w-full rounded-lg bg-np-yellow px-5 py-2.5 text-sm md:text-base lg:text-lg font-medium text-white hover:bg-np-yellow/90 focus:outline-none focus:ring-4 focus:ring-np-white transition duration-150 ease-in-out">
+                                    Bekijk occasions
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                @endfor
+            </div>
+        </div>
+    </section>
+@endsection
