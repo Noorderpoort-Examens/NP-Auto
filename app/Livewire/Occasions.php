eb48fb93c05f5929bf0e0789997c4e53d1f6daae
DanielD <ddobma1@st.noorderpoort.nl>
2025-05-15 11:16:46 +0200

    Created component for list of occasions

---
+++ app/Livewire/Occasions.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Occasion;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class Occasions extends Component
+{
+    use WithPagination;
+
+    public $is_archive = false;
+
+    public function render()
+    {
+        $occasions = $this->is_archive
+            ? Occasion::orderBy('created_at', 'desc')->paginate(12)
+            : Occasion::orderBy('created_at', 'desc')->take(4)->get(); // For home
+
+
+        return view('livewire.occasions', compact('occasions'));
+    }
+}
--- resources/views/occasions-archive.blade.php
+++ resources/views/occasions-archive.blade.php
@@ -11,41 +11,7 @@
                     <livewire:filter-modal-occasions />
                 </div>
             </div>
-
-            <div class="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-4">
-                @for ($i = 0; $i < 25; $i++)
-                    <div class="relative rounded-lg border p-6 shadow-sm border-gray-700 bg-np-dark/40 h-max">
-                        <a href="#" class="block group">
-                            <div
-                                class="relative overflow-hidden rounded-lg transform transition duration-150 ease-in-out group-hover:scale-110">
-                                <img src="{{ asset('img/a1_1.jpeg') }}" alt="logo" class="block w-full rounded-lg">
-                                <div
-                                    class="absolute top-0 left-0 bg-red-600 text-white text-sm font-bold py-1 px-3 z-10 rounded">
-                                    Verkocht
-                                </div>
-                            </div>
-                        </a>
-
-                        <div class="pt-6">
-                            <p
-                                class="text-sm md:text-base lg:text-lg font-semibold leading-tight hover:underline text-white">
-                                Audi A3 Sportback
-                            </p>
-                            <p
-                                class="text-sm md:text-base lg:text-lg font-semibold leading-tight hover:underline text-white">
-                                €20.000
-                            </p>
-
-                            <div class="mt-4 flex items-center justify-between gap-4">
-                                <button type="button"
-                                    class="items-center w-full rounded-lg bg-np-yellow px-5 py-2.5 text-sm md:text-base lg:text-lg font-medium text-white hover:bg-np-yellow/90 focus:outline-none focus:ring-4 focus:ring-np-white transition duration-150 ease-in-out">
-                                    Bekijk occasions
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                @endfor
-            </div>
+            <livewire:occasions />
         </div>
     </section>
 @endsection
