a96658ba66800c12ce1f93ed34663a3df1bd06e2
DanielD <ddobma1@st.noorderpoort.nl>
2025-05-15 10:33:52 +0200

    Add filter component in the occasions archive

--- resources/views/occasions-archive.blade.php
+++ resources/views/occasions-archive.blade.php
@@ -6,7 +6,10 @@
     <section class="py-8 antialiased bg-np-dark md:py-12">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="mb-4 items-end justify-between space-y-4 sm:flex sm:space-y-0 md:mb-8">
-                    <h2 class="mt-3 font-semibold text-np-white text-3xl md:text-4xl lg:text-5xl">Occasions</h2>
+                <h2 class="mt-3 font-semibold text-np-white text-3xl md:text-4xl lg:text-5xl">Occasions</h2>
+                <div class="flex items-center space-x-4">
+                    <livewire:filter-modal-occasions />
+                </div>
             </div>
 
             <div class="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-4">
