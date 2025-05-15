38799b7a786300af639345518ef9af4629e999fd
DanielD <ddobma1@st.noorderpoort.nl>
2025-05-15 14:21:50 +0200

    Restyle occasions archive

--- resources/views/occasions-archive.blade.php
+++ resources/views/occasions-archive.blade.php
@@ -3,10 +3,10 @@
 @section('title', 'Occasions Archive')
 
 @section('content')
-    <section class="py-8 antialiased bg-np-dark md:py-12">
+    <section class="py-8 antialiased md:py-12">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="mb-4 items-end justify-between space-y-4 sm:flex sm:space-y-0 md:mb-8">
-                <h2 class="mt-3 font-semibold text-np-white text-3xl md:text-4xl lg:text-5xl">Occasions</h2>
+                <h2 class="mt-3 font-semibold text-np-dark text-3xl md:text-4xl lg:text-5xl">Occasions</h2>
                 <div class="flex items-center space-x-4">
                     <livewire:filter-modal-occasions />
                 </div>
