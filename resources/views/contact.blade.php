4dadf8b781c15ca3c080d500e959b74220db00b7
DanielD <ddobma1@st.noorderpoort.nl>
2025-05-19 13:29:16 +0200

    Add contact form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -15,6 +15,35 @@
                 <iframe class="top-0 left-0 w-full h-full border border-np-dark rounded" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2389.803306596546!2d6.559752376671371!3d53.20344367224814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c832ae8b38fb37%3A0xf8ed82115525c5de!2sMuntinglaan%203%2C%209727%20JT%20Groningen!5e0!3m2!1snl!2snl!4v1747653022713!5m2!1snl!2snl" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
+
+        <div>
+            <h2 class="text-lg md:text-xl lg:text-2xl text-center font-medium font-roboto">Contact formulier</h2>
+            <form class="bg-np-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div class="mb-4">
+                    <label class="block text-np-dark text-sm md:text-base lg:text-lg font-bold mb-2 uppercase" for="name">
+                        Naam
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-np-dark leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Vul hier je naam in">
+                </div>
+                <div class="mb-4">
+                    <label class="block text-np-dark text-sm md:text-base lg:text-lg font-bold mb-2 uppercase" for="email">
+                        Email
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-np-dark leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Vul hier je email in">
+                </div>
+                <div class="mb-4">
+                    <label class="block text-np-dark text-sm md:text-base lg:text-lg font-bold mb-2 uppercase" for="username">
+                        Bericht
+                    </label>
+                    <textarea id="message" rows="4" class="shadow appearance-none border rounded w-full py-2 px-3 text-np-dark leading-tight focus:outline-none focus:shadow-outline" placeholder="Vul hier je bericht in..."></textarea>
+                </div>
+                <div class="flex items-center justify-center">
+                    <button class="bg-np-dark hover:bg-np-yellow text-white font-bold py-2 px-4 text-sm md:text-base lg:text-lg rounded focus:outline-none focus:shadow-outline transition duration-150 ease-in-out" type="submit">
+                        Verstuur contact aanvraag
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 
