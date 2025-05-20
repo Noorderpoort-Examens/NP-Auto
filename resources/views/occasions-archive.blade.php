473e7cd466d7c57a38b3e7cf074056fe29fb1bdf
DanielD <ddobma1@st.noorderpoort.nl>
2025-05-20 14:07:37 +0200

    Removed section tag

--- resources/views/occasions-archive.blade.php
+++ resources/views/occasions-archive.blade.php
@@ -3,7 +3,6 @@
 @section('title', 'Occasions Archive')
 
 @section('content')
-    <section class="py-8 antialiased md:py-12">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="mb-4 items-end justify-between space-y-4 sm:flex sm:space-y-0 md:mb-8">
                 <h2 class="mt-3 font-semibold text-np-dark text-3xl md:text-4xl lg:text-5xl">Occasions</h2>
@@ -13,5 +12,4 @@
             </div>
             <livewire:occasions :is_archive="true" />
         </div>
-    </section>
 @endsection
