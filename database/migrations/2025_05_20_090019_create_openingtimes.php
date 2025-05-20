5048a278278372170eb1a8ea7a3e42e38e05ae22
DanielD <ddobma1@st.noorderpoort.nl>
2025-05-20 13:56:39 +0200

    Name correct table for dropifexists

--- database/migrations/2025_05_20_090019_create_openingtimes.php
+++ database/migrations/2025_05_20_090019_create_openingtimes.php
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('reservations');
+        Schema::dropIfExists('opening_times');
     }
 };
