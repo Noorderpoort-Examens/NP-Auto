2ba1793b4a2bbb97071ea63aa1d46edab7782c9b
DanielD <ddobma1@st.noorderpoort.nl>
2025-05-20 14:07:58 +0200

    Fixed correct spacing

--- resources/views/occasions-archive.blade.php
+++ resources/views/occasions-archive.blade.php
@@ -3,13 +3,13 @@
 @section('title', 'Occasions Archive')
 
 @section('content')
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div class="mb-4 items-end justify-between space-y-4 sm:flex sm:space-y-0 md:mb-8">
-                <h2 class="mt-3 font-semibold text-np-dark text-3xl md:text-4xl lg:text-5xl">Occasions</h2>
-                <div class="flex items-center space-x-4">
-                    <livewire:filter-modal-occasions />
-                </div>
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 my-3 md:my-4 lg:my-5">
+        <div class="mb-4 items-end justify-between space-y-4 sm:flex sm:space-y-0 md:mb-8">
+            <h2 class="mt-3 font-semibold text-np-dark text-3xl md:text-4xl lg:text-5xl">Occasions</h2>
+            <div class="flex items-center space-x-4">
+                <livewire:filter-modal-occasions />
             </div>
-            <livewire:occasions :is_archive="true" />
         </div>
+        <livewire:occasions :is_archive="true" />
+    </div>
 @endsection
