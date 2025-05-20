effc51521ae4deda8782a25b3693f2a0d141d41a
DanielD <ddobma1@st.noorderpoort.nl>
2025-05-20 13:43:32 +0200

    Deleted unused defining use

--- app/Filament/Resources/OpeningTimeResource/Pages/CreateOpeningTime.php
+++ app/Filament/Resources/OpeningTimeResource/Pages/CreateOpeningTime.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\OpeningTimeResource\Pages;
 
 use App\Filament\Resources\OpeningTimeResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateOpeningTime extends CreateRecord
