36a4e4a12cc393fb2fa8914170f216a5ae0b8b2e
DanielD <ddobma1@st.noorderpoort.nl>
2025-05-21 15:17:22 +0200

    Add google maps view to footer

--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -31,6 +31,14 @@
                 <h2 class="mb-6 text-sm font-semibold text-np-dark uppercase">Contact details</h2>
                 <livewire:contact-details :is_contact_page="false" />
             </div>
+            <div>
+                <div class="relative w-full h-96 flex flex-col lg:justify-center left-0 right-0 lg:right-8">
+                    <iframe class="top-0 left-0 w-full h-full border border-np-dark rounded"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2389.803306596546!2d6.559752376671371!3d53.20344367224814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c832ae8b38fb37%3A0xf8ed82115525c5de!2sMuntinglaan%203%2C%209727%20JT%20Groningen!5e0!3m2!1snl!2snl!4v1747653022713!5m2!1snl!2snl"
+                        width="600" height="450" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
         </div>
         <div class="px-4 pb-6 md:flex md:items-center md:justify-center">
             <span class="text-sm text-np-dark/50 sm:text-center">© 2025
