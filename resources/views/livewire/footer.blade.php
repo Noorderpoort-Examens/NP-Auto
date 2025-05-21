93c8036ccf98c264e6f32c57dabbae37426c0b9d
DanielD <ddobma1@st.noorderpoort.nl>
2025-05-21 14:38:34 +0200

    Redesign footer with adding openingtimes and a redesign of showing the navigation links

--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -1,22 +1,37 @@
 <footer class="bg-np-white rounded-lg shadow-sm container-shadow">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full  p-4 md:flex md:items-center md:justify-between">
-        <span class="text-sm text-np-dark sm:text-center">
-            © 2025
-            <a href="{{ route('home') }}" class="hover:underline">NP-Auto</a>
-        </span>
-        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-            <x-nav-link :href="route('home')" wire:navigate>
-                {{ __('Home') }}
-            </x-nav-link>
-            <x-nav-link :href="route('occasions.archive')" wire:navigate>
-                {{ __('Occasions') }}
-            </x-nav-link>
-            <x-nav-link :href="route('home')" wire:navigate>
-                {{ __('Service & Onderhoud') }}
-            </x-nav-link>
-            <x-nav-link :href="route('contact')" wire:navigate>
-                {{ __('Contact') }}
-            </x-nav-link>
-        </ul>
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full p-4 ">
+        <div class="grid sm:grid-cols-2 gap-4 sm:gap-8 px-4 py-6 lg:py-8 md:grid-cols-2">
+            <div>
+                <h2 class="mb-6 text-sm font-semibold text-np-dark uppercase">Openingstijden</h2>
+                <livewire:opening-times />
+            </div>
+            <div>
+                <h2 class="mb-6 text-sm font-semibold text-np-dark uppercase">Navigatie links</h2>
+                <ul
+                    class="grid sm:grid-cols-2 items-center gap-4 mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                    <a class="hover:text-np-yellow transition duration-150 ease-in-out text-xs md:text-sm lg:text-base font-opensans text-nowrap text-np-dark"
+                        href="{{ route('home') }}">
+                        Home
+                    </a>
+                    <a class="hover:text-np-yellow transition duration-150 ease-in-out text-xs md:text-sm lg:text-base font-opensans text-nowrap text-np-dark"
+                        href="{{ route('home') }}">
+                        Service & Onderhoud
+                    </a>
+                    <a class="hover:text-np-yellow transition duration-150 ease-in-out text-xs md:text-sm lg:text-base font-opensans text-nowrap text-np-dark"
+                        href="{{ route('occasions.archive') }}">
+                        Occasions
+                    </a>
+                    <a class="hover:text-np-yellow transition duration-150 ease-in-out text-xs md:text-sm lg:text-base font-opensans text-nowrap text-np-dark"
+                        href="{{ route('contact') }}">
+                        Contact
+                    </a>
+                </ul>
+            </div>
+        </div>
+        <div class="px-4 pb-6 md:flex md:items-center md:justify-center">
+            <span class="text-sm text-np-dark/50 sm:text-center">© 2025
+                <a class="hover:underline" href="{{ route('home') }}">NP-Auto</a>. All Rights Reserved.
+            </span>
+        </div>
     </div>
 </footer>
