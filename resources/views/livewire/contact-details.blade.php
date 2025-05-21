31143a56c02b29b70a9820a5a6e3173dc187de43
DanielD <ddobma1@st.noorderpoort.nl>
2025-05-21 15:15:53 +0200

    Setup flex or grid  layout depends on is_contact_page true or false

--- resources/views/livewire/contact-details.blade.php
+++ resources/views/livewire/contact-details.blade.php
@@ -1,5 +1,6 @@
-<div class="flex gap-4 justify-center">
-    <span class="flex gap-0.5 items-center justify-center font-opensans font-normal text-xs md:text-sm lg:text-base">
+<div class="gap-4 {{ $is_contact_page ? ' flex justify-center' : ' justify-start grid sm:grid-cols-2' }}">
+
+    <span class="flex gap-0.5 items-center justify-left font-opensans font-normal text-xs md:text-sm lg:text-base">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
             class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round"
@@ -8,7 +9,7 @@
         {!! $phonenumber !!}
     </span>
 
-    <span class="flex gap-0.5 items-center justify-center font-opensans font-normal text-xs md:text-sm lg:text-base">
+    <span class="flex gap-0.5 items-center justify-left font-opensans font-normal text-xs md:text-sm lg:text-base">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
             stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round"
@@ -16,4 +17,26 @@
         </svg>
         {!! $email !!}
     </span>
+
+    @if (!$is_contact_page)
+        <span class="flex gap-0.5 items-center justify-left font-opensans font-normal text-xs md:text-sm lg:text-base">
+
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+            </svg>
+
+            {!! $adress !!}
+        </span>
+        <span class="flex gap-0.5 items-center justify-left font-opensans font-normal text-xs md:text-sm lg:text-base">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+            </svg>
+            {!! $zipcode !!}
+        </span>
+    @endif
 </div>
