d0bb2e906e0db497e3ad6feeb573827230d69a5e
Remco de Vries <remco-de-vries@ziggo.nl>
2025-05-21 15:52:55 +0200

    added regex message validation

--- resources/lang/nl/validation.php
+++ resources/lang/nl/validation.php
@@ -6,6 +6,7 @@ return [
     'unique' => 'De :attribute is al in gebruik.',
     'integer' => 'Ongeldig nummer.',
     'numeric' => 'Ongeldig nummer.',
+    'regex' => 'Ongeldig telefoon nummer.', //if ever regex is used in other places it will need to be specified, but currently only phonenumber validates with regex classification
     'mimetypes' => 'Er is iets misgegaan, probeer de images opnieuw te sturen',
     'boolean' => 'Er is iets misgegaan, geen correcte bool value gevonden.',
     'before' => 'Er is iets misgegaan, de tijd hoort eerder te zijn dan de opgegeven eindtijd.',
