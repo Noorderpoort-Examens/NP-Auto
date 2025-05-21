a01166d97a0dd9872c62b3ff332584235dac6e1e
Remco de Vries <remco-de-vries@ziggo.nl>
2025-05-21 16:20:35 +0200

    temp commit to review quickly for daniël

--- resources/views/livewire/service-list.blade.php
+++ resources/views/livewire/service-list.blade.php
@@ -1,4 +1,49 @@
-<div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+<div class="mb-4 grid gap-4 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+
+
+    @foreach ($services as $service)
+    <div class="relative rounded-lg p-6 shadow-sm bg-np-dark/90 h-max">
+        <a href="#" class="block group">
+            <h1 class="font-roboto text-lg md:text-xl lg:text-2xl font-semibold leading-tight text-white">
+                {{ $service->title }}
+            </h1>
+        </a>
+
+        <div class="pt-3">
+            <p class="text-sm md:text-base lg:text-lg font-semibold leading-tight text-white">
+                {{ $service->description }}
+            </p>
+            <p class="text-sm md:text-base lg:text-lg font-semibold leading-tight hover:underline text-white">
+                momgae
+            </p>
+
+            <div class="mt-4 flex items-center justify-between gap-4">
+                hee hee hee ha
+            </div>
+        </div>
+    </div>
+    @endforeach
+
+    <div class="relative rounded-lg p-6 shadow-sm bg-np-dark/90 h-max">
+        <a href="#" class="block group">
+            <h1 class="font-roboto text-sm md:text-base lg:text-lg font-semibold leading-tight text-white">
+                me when ur mom
+            </h1>
+        </a>
+
+        <div class="pt-6">
+            <p class="text-sm md:text-base lg:text-lg font-semibold leading-tight text-white">
+                me when ur mom
+            </p>
+            <p class="text-sm md:text-base lg:text-lg font-semibold leading-tight hover:underline text-white">
+                momgae
+            </p>
+
+            <div class="mt-4 flex items-center justify-between gap-4">
+                hee hee hee ha
+            </div>
+        </div>
+    </div>
     @foreach ($services as $service)
         <div class="border rounded-xl shadow p-4 bg-white">
             <h2 class="text-lg font-semibold text-np-dark">{{ $service->title }}</h2>
