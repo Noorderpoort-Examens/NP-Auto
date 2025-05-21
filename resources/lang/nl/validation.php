67b36d379530afab6d9b53d83383c1338cb9b797
Remco de Vries <remco-de-vries@ziggo.nl>
2025-05-21 15:44:48 +0200

    numeric validation

--- resources/lang/nl/validation.php
+++ resources/lang/nl/validation.php
@@ -5,6 +5,7 @@ return [
     'email' => 'Het veld :attribute moet een geldig e-mailadres zijn.',
     'unique' => 'De :attribute is al in gebruik.',
     'integer' => 'Ongeldig nummer.',
+    'numeric' => 'Ongeldig nummer.',
     'mimetypes' => 'Er is iets misgegaan, probeer de images opnieuw te sturen',
     'boolean' => 'Er is iets misgegaan, geen correcte bool value gevonden.',
     'before' => 'Er is iets misgegaan, de tijd hoort eerder te zijn dan de opgegeven eindtijd.',
