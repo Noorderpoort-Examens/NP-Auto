f655489018a4676e8f70d5c3404e63786bcb3165
DanielD <ddobma1@st.noorderpoort.nl>
2025-05-21 11:40:34 +0200

    Add recent occasions section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,4 +3,19 @@
 @section('title', 'Welkom')
 
 @section('content')
+
+<section>
+    <div class="mx-auto px-4 sm:px-6 lg:px-8 my-3 md:my-4 lg:my-5">
+        <h1 class="text-3xl md:text-4xl lg:text-5xl text-center mb-5 font-roboto font-bold">
+            Meest recente occasions
+        </h1>
+
+        <livewire:occasions :is_archive="false" />
+
+        <div class="w-full flex  flex-col justify-center items-center">
+            <a class="w-full md:w-max text-center text-sm md:text-base lg:text-lg bg-np-yellow rounded-lg font-opensans font-semibold text-np-white px-16 py-3.5 md:px-24 md:py-4 lg:px-32 lg:py-4 hover:bg-np-dark hover:text-np-yellow transition duration-150 ease-in-out" href="{{route('occasions.archive')}}">Meer occasions</a>
+        </div>
+    </div>
+</section>
+
 @endsection
