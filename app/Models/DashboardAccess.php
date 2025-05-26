f51d840d871eeeae6100114e3d8d4f185c1e9296
DanielD <ddobma1@st.noorderpoort.nl>
2025-05-26 11:50:45 +0200

    Add dashboardaccess model belongsTo Role

--- app/Models/DashboardAccess.php
+++ app/Models/DashboardAccess.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Models\Role;
 
 class DashboardAccess extends Model
 {
-    protected $fillable = [
-        'role_name',
-        'can_access',
-    ];
+    protected $fillable = ['role_id', 'can_access'];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
